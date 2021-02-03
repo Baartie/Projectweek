28be6afe5e3c377e2858c33577923795d356d76f
RensBaart <327595@student.mboutrecht.nl>
2021-02-03 10:18:19 +0100

    Change: De basis HTML5 - Bootstrap pagina waar de stukjes script naar een apart bestand wijzen. Nog geen content.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+  <head>
+<!-- Including de head. !-->
+<?php include'./head.php'; ?>
+  </head>
+  <body>
+
+
+
+<!-- Bootstrap scripts !-->
+<?php include'./bsscripts.php'; ?>
+
+  </body>
+</html>
