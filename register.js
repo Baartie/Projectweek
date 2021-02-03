1eb41e5dac304d2e383b0ee67a63d1dddd5e5139
RensBaart <327595@student.mboutrecht.nl>
2021-02-03 20:38:10 +0100

    Added: JS die bij de 'register' pagina hoort

---
+++ register.js
@@ -0,0 +1,14 @@
+var myApp = angular.module("myApp", []);
+myApp.controller("RegisterCtrl", function ($scope) {
+
+});
+
+var data = dataOpslag();
+
+data =- Opslag;
+
+print(data);
+
+var email = ValidityState (true)
+
+if email.ValidityState  $error.email
