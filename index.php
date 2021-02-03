a6c4a7758295ebd32a928300321386613ca1788b
RensBaart <327595@student.mboutrecht.nl>
2021-02-03 11:52:01 +0100

    Change: Link naar CSS toegevoegd, HTML navbar aangebracht incl Links.

--- index.php
+++ index.php
@@ -4,11 +4,34 @@
 <head>
     <!-- Including de head. !-->
     <?php include'./head.php'; ?>
+    <link rel="stylesheet" href="assets/style/style.css">
 </head>
 
 <body>
 
+    <div class="nav" id="navbar-kleuren">
+        <input type="checkbox" id="nav-check">
+        <div class="nav-header">
+            <div class="nav-title">
+                CSGO-Info
+            </div>
+        </div>
+        <div class="nav-btn">
+            <label for="nav-check">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+        </div>
 
+        <div class="nav-links">
+            <a href="#" target="_blank">Home</a>
+            <a href="#" target="_blank">Tactics</a>
+            <a href="#" target="_blank">Smokes/Grenades</a>
+            <a href="#" target="_blank">Info</a>
+            <a id="login-knop" href="http://">login</a>
+        </div>
+    </div>
 
     <!-- Bootstrap scripts !-->
     <?php include'./bsscripts.php'; ?>
