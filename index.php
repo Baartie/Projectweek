0e143c71ebf14c308f8ea9dd7060efd0606dab31
RensBaart <327595@student.mboutrecht.nl>
2021-02-03 10:47:15 +0100

    Change: Geformat in HTML

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <!doctype html>
 <html lang="en">
-  <head>
-<!-- Including de head. !-->
-<?php include'./head.php'; ?>
-  </head>
-  <body>
+
+<head>
+    <!-- Including de head. !-->
+    <?php include'./head.php'; ?>
+</head>
+
+<body>
 
 
 
-<!-- Bootstrap scripts !-->
-<?php include'./bsscripts.php'; ?>
+    <!-- Bootstrap scripts !-->
+    <?php include'./bsscripts.php'; ?>
 
-  </body>
+</body>
+
 </html>
