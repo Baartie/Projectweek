3d28aaec3422794b3c054b1e057ba246da8efb89
RensBaart <327595@student.mboutrecht.nl>
2021-02-03 20:37:33 +0100

    Added: ZigZag stijl van cards en informatie

--- index.php
+++ index.php
@@ -28,7 +28,8 @@
             <a href="#" target="_blank">Home</a>
             <a href="#" target="_blank">Tactics</a>
             <a href="#" target="_blank">Smokes/Grenades</a>
-            <a href="#" target="_blank">Info</a>
+            <a href="./contact.php
+            " target="_blank">Contact</a>
             <a id="login-knop" href=./login-page.html>login</a>
         </div>
     </div>
@@ -37,6 +38,64 @@
         <img src="https://goexanimo.lv/storage/images/news/lg_csgo-2x2-turnirs-8-VUl54fQRkz.png" alt="">
         <h1 class="hoofdtekst">WELCOME!</h1>
     </div>
+
+
+    <!-- The App Section -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="column-66">
+                <h1 class="xlarge-font"><b>CSGO Tips</b></h1>
+                <h1 class="large-font" style="color:MediumSeaGreen;"></h1>
+                <p class="paragraph1">You should buy this app because lorem
+                    ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                    commodo consequat.</p>
+                <button class="button">Download Application</button>
+            </div>
+            <div class="column-33">
+                <img src="https://www.w3schools.com/w3images/img_app.jpg" width="335" height="471">
+            </div>
+        </div>
+    </div>
+
+    <!-- Clarity Section -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="column-33">
+                <img src="https://www.w3schools.com/w3images/app5.jpg" alt="App" width="335" height="471">
+            </div>
+            <div class="column-66">
+                <h1 class="xlarge-font"><b>Testkopje</b></h1>
+                <p class="paragraph1"> Sharp and clear photos with the
+                    world's best photo engine, incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquipex ea commodo consequat. Duis aute irure
+                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <button class="button">Read More</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- The App Section -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="column-66">
+                <h1 class="xlarge-font"><b>Testkopje2</b></h1>
+                <p class="paragraph1"> ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+                <button class="button">Download Application</button>
+            </div>
+            <div class="column-33">
+                <img src="https://www.w3schools.com/w3images/img_app.jpg" width="335" height="471">
+            </div>
+        </div>
+    </div>
+
+
+
+
+
 
     <!--Footer-->
     <div class="indexfooter">
