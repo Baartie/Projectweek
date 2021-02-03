c68c1d433a8252d13261104cfa27dd2434faeb97
RensBaart <327595@student.mboutrecht.nl>
2021-02-03 21:26:39 +0100

    Added: Eerste versie van de login pagina werkend!

--- login.php
+++ login.php
@@ -2,25 +2,48 @@
 <html>
 
 <head>
-    <?php include'head.php'; ?>
-    <link rel="stylesheet" href="assets\style\style.css">
+    <meta charset="utf-8"/>
+    <title>Login</title>
+    <link rel="stylesheet" href="assets\style\loginpage.css"/>
 </head>
 
 <body>
-    <div class="wrapper">
-        <form class="form-signin">
-            <h2 class="form-signin-heading">Please login</h2>
-            <input type="text" class="form-control" name="username" placeholder="Email Address" required=""
-                autofocus="" />
-            <input type="password" class="form-control" name="password" placeholder="Password" required="" />
-            <label class="checkbox">
-                <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
-            </label>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
-        </form>
-    </div>
-
-    <?php include'bsscripts.php'; ?>
+<?php
+    require('db.php');
+    session_start();
+    // When form submitted, check and create user session.
+    if (isset($_POST['username'])) {
+        $username = stripslashes($_REQUEST['username']);    // removes backslashes
+        $username = mysqli_real_escape_string($con, $username);
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($con, $password);
+        // Check user is exist in the database
+        $query    = "SELECT * FROM `users` WHERE username='$username'
+                     AND password='" . md5($password) . "'";
+        $result = mysqli_query($con, $query) or die(mysql_error());
+        $rows = mysqli_num_rows($result);
+        if ($rows == 1) {
+            $_SESSION['username'] = $username;
+            // Redirect to user dashboard page
+            header("Location: dashboard.php");
+        } else {
+            echo "<div class='form'>
+                  <h3>Incorrect Username/password.</h3><br/>
+                  <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
+                  </div>";
+        }
+    } else {
+?>
+    <form class="form" method="post" name="login">
+        <h1 class="login-title">Login</h1>
+        <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
+        <input type="password" class="login-input" name="password" placeholder="Password"/>
+        <input type="submit" value="Login" name="submit" class="login-button"/>
+        <p class="link"><a href="./registration.php">New Registration</a></p>
+  </form>
+<?php
+    }
+?>
 </body>
 
 </html>
