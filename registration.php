6a05c1cc94b1864524595eae32db6f15415b5da8
RensBaart <327595@student.mboutrecht.nl>
2021-02-04 11:51:15 +0100

    Change: aanpassing Required fields,

--- registration.php
+++ registration.php
@@ -1,29 +1,40 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="utf-8"/>
+    <meta charset="utf-8" />
     <title>Registration</title>
-    <link rel="stylesheet" href="assets\style\loginpage.css"/>
+    <link rel="stylesheet" href="assets\style\loginpage.css" />
 </head>
 
 <body>
 
-<?php
-    require('db.php');
-    // When form submitted, insert values into the database.
-    if (isset($_REQUEST['username'])) {
-        // removes backslashes
-        $username = stripslashes($_REQUEST['username']);
-        //escapes special characters in a string
-        $username = mysqli_real_escape_string($con, $username);
-        $email    = stripslashes($_REQUEST['email']);
-        $email    = mysqli_real_escape_string($con, $email);
-        $password = stripslashes($_REQUEST['password']);
-        $password = mysqli_real_escape_string($con, $password);
-        $create_datetime = date("Y-m-d H:i:s");
-        $query    = "INSERT into `users` (username, password, email, create_datetime)
-                     VALUES ('$username', '" . md5($password) . "', '$email', '$create_datetime')";
-        $result   = mysqli_query($con, $query);
+    <?php
+        require('db.php');
+            $username = mysqli_real_escape_string($con, $username);
+            $email    = stripslashes($_REQUEST['email']);
+            $email    = mysqli_real_escape_string($con, $email);
+            $password = stripslashes($_REQUEST['password']);
+            $password = mysqli_real_escape_string($con, $password);
+            $create_datetime = date("Y-m-d H:i:s");
+            $query    = "INSERT into `users` (username, password, email, create_datetime)
+                         VALUES ('$username', '" . md5($password) . "', '$email', '$create_datetime')";
+            $result   = mysqli_query($con, $query);
+
+    if (isset($_REQUEST['username'])) 
+    {
+        if (isset($_POST['user']))
+        {
+            $user = $_POST['user'];
+            if (mysql_num_rows(yourFunctionForQueryingMySQL("SELECT * FROM users WHERE username='$user'")))
+                 echo "<div class='form'>
+                 <h3>Username is taken.</h3><br/>
+                 <h4>Try another username.</h4>
+                 </div>";
+
+            else $username = stripslashes($_REQUEST['username']);
+            
+
         if ($result) {
             echo "<div class='form'>
                   <h3>You are registered successfully.</h3><br/>
@@ -35,18 +46,23 @@
                   <p class='link'>Click here to <a href='registration.php'>registration</a> again.</p>
                   </div>";
         }
-    } else {
+        }
+    }
+
+
+
+
 ?>
     <form class="form" action="" method="post">
         <h1 class="login-title">Registration</h1>
         <input type="text" class="login-input" name="username" placeholder="Username" required />
-        <input type="text" class="login-input" name="email" placeholder="Email Adress">
-        <input type="password" class="login-input" name="password" placeholder="Password">
+        <input type="text" class="login-input" name="email" placeholder="Email Adress" required />
+        <input type="password" class="login-input" name="password" placeholder="Password" required />
         <input type="submit" name="submit" value="Register" class="login-button">
         <p class="link"><a href="./login.php">Click to Login</a></p>
     </form>
-<?php
-    }
+    <?php
+
 ?>
 </body>
 
