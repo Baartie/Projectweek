8498f7316af1ba630a2f30d1118503edfb188822
RensBaart <327595@student.mboutrecht.nl>
2021-02-04 21:29:35 +0100

    Added: Sticky-header and Welcome login message

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: login.php");
+  }
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -9,28 +23,28 @@
 </head>
 
 <body>
+    <nav class="sticky-top shadow-lg ">
+        <div class="nav " id="navbar-kleuren">
+            <input type="checkbox" id="nav-check">
+            <div class="nav-header">
+                <div class="nav-title">CSGO-FAMILY.COM</div>
+            </div>
+            <div class="nav-btn">
+                <label for="nav-check">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </label>
+            </div>
 
-    <div class="nav " id="navbar-kleuren">
-        <input type="checkbox" id="nav-check">
-        <div class="nav-header">
-            <div class="nav-title">CSGO-FAMILY.COM</div>
+            <div class="nav-links">
+                <a href="#">Home</a>
+                <a href="./contact.php">Contact</a>
+                <a href="#">Profile</a>
+                <a id="login-knop" class="" href=./login.php>login</a>
+            </div>
         </div>
-        <div class="nav-btn">
-            <label for="nav-check">
-                <span></span>
-                <span></span>
-                <span></span>
-            </label>
-        </div>
-
-        <div class="nav-links">
-            <a href="#">Home</a>
-            <a href="./contact.php">Contact</a>
-            <a href="#">Profile</a>
-            <a id="login-knop" class="" href=./login.php>login</a>
-        </div>
-    </div>
-
+    </nav>
     <div style="height: 500px;" class="box-shadow">
         <img src="https://goexanimo.lv/storage/images/news/lg_csgo-2x2-turnirs-8-VUl54fQRkz.png" alt="">
         <h1 class="hoofdtekst">WELCOME!</h1>
@@ -50,10 +64,11 @@
                     world's best photo engine, incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                     quis nostrud exercitation ullamco laboris nisi ut aliquipex ea commodo consequat. Duis aute irure
                     dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                    <a class="button-links" href="./esports.php">Click here!</a>
+                <a class="button-links" href="./esports.php">Click here!</a>
             </div>
             <div class="column-33">
-                <img src="https://www.seekpng.com/png/full/121-1217542_steams-gemenskap-scream-cs-go-hd.png" width="400" height="400">
+                <img src="https://www.seekpng.com/png/full/121-1217542_steams-gemenskap-scream-cs-go-hd.png" width="400"
+                    height="400">
             </div>
         </div>
     </div>
@@ -62,18 +77,20 @@
     <div class="container-fluid">
         <div class="row">
             <div class="column-33">
-                <img src="https://www.pngkey.com/png/full/149-1499168_csgo-knife-knifes-csgo-knife-in-hand.png" width="100%" height="100%">
+                <img src="https://www.pngkey.com/png/full/149-1499168_csgo-knife-knifes-csgo-knife-in-hand.png"
+                    width="100%" height="100%">
             </div>
             <div class="column-66">
                 <h1 class="xlarge-font"><b>Skins</b></h1>
                 <p class="paragraph1"> Sharp and clear photos with the
                     world's best photo engine, incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                     quis nostrud exercitation ullamco laboris nisi ut aliquipex ea commodo consequat. Duis aute irure
-                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Sharp and clear photos with the
+                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Sharp and
+                    clear photos with the
                     world's best photo engine, incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                     quis nostrud exercitation ullamco laboris nisi ut aliquipex ea commodo consequat. Duis aute irure
                     dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                    <a class="button-links" href="./skins.php">Click here!</a>
+                <a class="button-links" href="./skins.php">Click here!</a>
             </div>
         </div>
     </div>
@@ -83,14 +100,15 @@
         <div class="row">
             <div class="column-66">
                 <h1 class="xlarge-font"><b>Gamemodes</b></h1>
-                <p class="paragraph1"> ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                <p class="paragraph1"> ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna
                     aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
                     commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur. Sharp and clear photos with the
                     world's best photo engine, incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                     quis nostrud exercitation ullamco laboris nisi ut aliquipex ea commodo consequat. Duis aute irure
                     dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                    <a class="button-links" href="./gamemodes.php">Click here!</a>
+                <a class="button-links" href="./gamemodes.php">Click here!</a>
             </div>
             <div class="column-33">
                 <img src="http://pngimg.com/uploads/counter_strike/counter_strike_PNG21.png" width="100%" height="100%">
